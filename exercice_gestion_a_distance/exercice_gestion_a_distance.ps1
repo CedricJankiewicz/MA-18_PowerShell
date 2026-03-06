ee138b4d00c5b67c067799ae5f4cead0c113ffb7
cedric <pm14cau@eduvaud.ch>
2026-03-06 13:54:24 +0100

    renamed file to more logical name and made exercise file

---
+++ exercice_gestion_a_distance/exercice_gestion_a_distance.ps1
@@ -0,0 +1,15 @@
+# Informations de connexion au PC client
+$computerName = 'PCMA18CLIENT'
+$username = 'Admin'
+$password = 'password'
+
+# TODO : établir la connexion au PC client
+
+# TODO : copier le fichier ZIP "DesktopGoose v0.3.zip" sur le client
+
+# TODO : extraire le contenu du fichier ZIP sur le client
+
+# Chemin vers PsExec sur la machine locale
+$PsExec = "PsExec.exe"
+
+# TODO : lancer l'application "GooseDesktop.exe" sur le client
